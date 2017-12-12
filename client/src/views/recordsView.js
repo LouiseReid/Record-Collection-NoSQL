4c02f75548d99a362c6b6fa1b115c65d904cc6ee
LouiseReid <l_reid@hotmail.co.uk>
2017-12-12 22:05:17 +0000

    updated to handle empty entries

--- client/src/views/recordsView.js
+++ client/src/views/recordsView.js
@@ -8,9 +8,11 @@ RecordsView.prototype = {
       var ul = document.getElementById('records')
       var li = document.createElement('li')
       var text = document.createElement('p')
+      if(record.artist && record.title !== " "){
       text.innerText = `${record.artist}: ${record.title}`
       li.appendChild(text)
       ul.appendChild(li)
+    }
     })
   }
 }
